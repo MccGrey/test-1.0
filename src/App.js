43f1569a2d7fa53fddcc3688d5c1bb21eae96a49
MccGrey <chuxzzie01@yahoo.com>
2022-11-04 03:38:21 +0100

    add pages

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import "./App.css";
 import Navbar from "./components/navbar/Navbar";
+import AlcoholicDrinks from "./pages/AlcoholicDrinks";
+import NonAlcoholicDrinks from "./pages/NonAlcoholicDrinks";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <>
       <Navbar />
+      <div className="container">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/alcoholicDrinks" element={<AlcoholicDrinks />} />
+          <Route path="/nonAlcoholicDrinks" element={<NonAlcoholicDrinks />} />
+        </Routes>
+      </div>
     </>
   );
 }
